fdbd9a71bdc9a5ccc6cd694442b06220f07b76b9
Santiago Serrano <santiagoandressp@ufps.edu.co>
2021-10-15 16:29:43 -0500

    Cambios en el modelo realizados

---
+++ src/main/java/com/example/demo/model/Color.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author santi
+ */
+@Entity
+@Table(name = "color")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Color.findAll", query = "SELECT c FROM Color c"),
+    @NamedQuery(name = "Color.findByIdColor", query = "SELECT c FROM Color c WHERE c.idColor = :idColor"),
+    @NamedQuery(name = "Color.findByNombre", query = "SELECT c FROM Color c WHERE c.nombre = :nombre")})
+public class Color implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_color")
+    private Integer idColor;
+    @Size(max = 25)
+    @Column(name = "nombre")
+    private String nombre;
+    @JoinTable(name = "producto_color", joinColumns = {
+        @JoinColumn(name = "id_color", referencedColumnName = "id_color")}, inverseJoinColumns = {
+        @JoinColumn(name = "id_producto", referencedColumnName = "id_producto")})
+    @ManyToMany
+    private Collection<Producto> productoCollection;
+
+    public Color() {
+    }
+
+    public Color(Integer idColor) {
+        this.idColor = idColor;
+    }
+
+    public Integer getIdColor() {
+        return idColor;
+    }
+
+    public void setIdColor(Integer idColor) {
+        this.idColor = idColor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public Collection<Producto> getProductoCollection() {
+        return productoCollection;
+    }
+
+    public void setProductoCollection(Collection<Producto> productoCollection) {
+        this.productoCollection = productoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idColor != null ? idColor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Color)) {
+            return false;
+        }
+        Color other = (Color) object;
+        if ((this.idColor == null && other.idColor != null) || (this.idColor != null && !this.idColor.equals(other.idColor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.example.demo.model.Color[ idColor=" + idColor + " ]";
+    }
+    
+}
